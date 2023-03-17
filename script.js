b9ae634563ccaf48c40af71b17a2847070df8e89
Hurtik Igor <hurtik.igor@gmail.com>
2023-03-17 11:48:11 +0300

    first commmmit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
