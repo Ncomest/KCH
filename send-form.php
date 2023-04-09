96ba778d7db6bfdbc397a173d0d39541d542c3df
Hurtik Igor <hurtik.igor@gmail.com>
2023-04-09 18:07:15 +0300

    change

--- send-form.php
+++ send-form.php
@@ -3,7 +3,6 @@ if(isset($_POST['submit'])) {
     $userName = $_POST['userName'];
     $userPhone = $_POST['userPhone'];
     $email = $_POST['email'];
-    $agree = $_POST['agree'];
     $to = "eledan_007@mail.ru";
     $subject = "Заявка с сайта";
     $message = "Имя: ".$userName."\nТелефон: ".$userPhone."\nE-mail: ".$email."\nПолитика конфиденциальности: ".$agree;
